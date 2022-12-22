6f992ff98e6d7b494b77a0232c540877b17f05bb
pirlruc <pirlruc@gmail.com>
2022-12-22 21:32:07 +0000

    Added variant type traits

--- include/improc/improc_function_traits.hpp
+++ include/improc/improc_function_traits.hpp
@@ -6,13 +6,15 @@ namespace improc {
     struct function_traits;
     
     // Function pointer
-    template<class ReturnType, class... Args>
+    template<class ReturnType, class ... Args>
     struct function_traits<ReturnType(*)(Args...)> : public function_traits<ReturnType(Args...)> {};
     
-    template<class ReturnType, class... Args>
-    struct function_traits<ReturnType(Args...)>
+    template<class ReturnType, class ... Args>
+    struct function_traits<ReturnType(Args ...)>
     {
         using return_type = ReturnType;
+
+        using signature = ReturnType(Args ...);
     
         static constexpr std::size_t arity = sizeof...(Args);
     
@@ -20,21 +22,20 @@ namespace improc {
         struct argument
         {
             static_assert(ArgSize < arity, "Invalid number of parameters for functor");
-            using type = typename std::tuple_element<ArgSize,std::tuple<Args...>>::type;
+            using type = typename std::tuple_element<ArgSize,std::tuple<Args ...>>::type;
         };
     };
 
     // Member function pointer
-    template<class ClassType, class ReturnType, class... Args>
-    struct function_traits<ReturnType(ClassType::*)(Args...)> : public function_traits<ReturnType(ClassType&,Args...)> {};
+    template<class ClassType, class ReturnType, class ... Args>
+    struct function_traits<ReturnType(ClassType::*)(Args ...)> : public function_traits<ReturnType(ClassType&,Args ...)> {};
     
-    template<class ClassType, class ReturnType, class... Args>
-    struct function_traits<ReturnType(ClassType::*)(Args...) const> : public function_traits<ReturnType(ClassType&,Args...)> {};
+    template<class ClassType, class ReturnType, class ... Args>
+    struct function_traits<ReturnType(ClassType::*)(Args ...) const> : public function_traits<ReturnType(ClassType&,Args ...)> {};
     
     // Member object pointer
     template<class ClassType, class ReturnType>
-    struct function_traits<ReturnType(ClassType::*)> : public function_traits<ReturnType(ClassType&)>
-    {};
+    struct function_traits<ReturnType(ClassType::*)> : public function_traits<ReturnType(ClassType&)> {};
 
     // Functor
     template<class Functor>
@@ -42,9 +43,12 @@ namespace improc {
     {
         private:
             using call_type = function_traits<decltype(&std::remove_reference<Functor>::type::operator())>;
+
         public:
             using return_type = typename call_type::return_type;
-        
+
+            using signature   = typename call_type::signature;
+
             static constexpr std::size_t arity = call_type::arity - 1;
     
             template <std::size_t ArgSize>
@@ -60,6 +64,20 @@ namespace improc {
     
     template<class Functor>
     struct function_traits<Functor&&> : public function_traits<Functor> {};
+
+    template <class Functor>
+    inline constexpr bool is_function_v = std::is_function<typename improc::function_traits<Functor>::signature>::value;
+
+    // Variant Functor
+    template<class>
+    struct is_variant_arg_function_type : std::false_type {};
+
+    template<class ... VariantArgs>
+    struct is_variant_arg_function_type<std::variant<VariantArgs ...>> 
+    : std::conjunction< std::is_function<typename improc::function_traits<VariantArgs>::signature> ...> {};
+
+    template<class T>
+    inline constexpr bool is_variant_arg_function_type_v = is_variant_arg_function_type<T>::value;
 }
 
 #endif
--- include/improc/improc_type_traits.hpp
+++ include/improc/improc_type_traits.hpp
@@ -9,15 +9,14 @@ namespace improc {
     inline constexpr bool dependent_false_v = false;
 
     template <class T>
-    struct is_folder_iterator : std::integral_constant  < bool
-                                                        ,    std::is_same<T, std::filesystem::recursive_directory_iterator>::value
-                                                          || std::is_same<T, std::filesystem::directory_iterator>::value > {};
+    struct is_folder_iterator : std::bool_constant<    std::is_same<T, std::filesystem::recursive_directory_iterator>::value
+                                                    || std::is_same<T, std::filesystem::directory_iterator>::value > {};
 
     template<class T>
     inline constexpr bool is_folder_iterator_v = is_folder_iterator<T>::value;
 
     template <class T>
-    struct is_hashable : std::integral_constant<bool, std::is_integral<T>::value || std::is_same<T, std::string>::value> {};
+    struct is_hashable : std::bool_constant<std::is_integral<T>::value || std::is_same<T, std::string>::value> {};
 
     template<class T>
     inline constexpr bool is_hashable_v = is_hashable<T>::value;
