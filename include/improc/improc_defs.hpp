f999a97bd69c854a45910d0fb5c016c0c1ac789b
pirlruc <3945588+pirlruc@users.noreply.github.com>
2025-05-05 22:08:10 +0000

    Refactor CMake configuration for shared/static library handling and improve test properties

--- include/improc/improc_defs.hpp
+++ include/improc/improc_defs.hpp
@@ -1,18 +1,18 @@
 #ifndef IMPROC_INFRASTRUCTURE_DEFINITIONS_HPP
 #define IMPROC_INFRASTRUCTURE_DEFINITIONS_HPP
 
-#ifndef IMPROC_API
-    #if defined _WIN32 || defined WINCE
+#if defined(IMPROC_STATIC)
+    #define IMPROC_API
+#elif defined(_WIN32) || defined(_WIN64) || defined(WINCE)
+    #if defined(IMPROC_EXPORTS)     // Building the DLL
         #define IMPROC_API __declspec(dllexport)
-    #elif defined __GNUC__ && __GNUC__ >= 4 || defined __APPLE__
-        #define IMPROC_API __attribute__ ((visibility ("default")))
-    #else
-        #define IMPROC_API
-    #endif
-#else
-    #if defined _WIN32 || defined WINCE
+    #else                           // Using the DLL
         #define IMPROC_API __declspec(dllimport)
     #endif
+#elif (defined(__GNUC__) && __GNUC__ >= 4) || defined(__clang__)
+    #define IMPROC_API __attribute__ ((visibility ("default")))
+#else
+    #define IMPROC_API
 #endif
 
 #define IMPROC_ENUM_KEY_TYPE unsigned int
