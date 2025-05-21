0b9c4577e038150d3e8ababbecf292a9658be70c
pirlruc <3945588+pirlruc@users.noreply.github.com>
2025-05-21 21:55:17 +0000

    Add missing template export definitions for static and dynamic builds

--- include/improc/improc_defs.hpp
+++ include/improc/improc_defs.hpp
@@ -3,16 +3,26 @@
 
 #if defined(IMPROC_STATIC)
     #define IMPROC_API
+    #define IMPROC_EXPORT_TEMPLATE_DECLARATION
+    #define IMPROC_EXPORT_TEMPLATE_DEFINITION
 #elif defined(_WIN32) || defined(_WIN64) || defined(WINCE)
     #if defined(IMPROC_EXPORTS)     // Building the DLL
         #define IMPROC_API __declspec(dllexport)
+        #define IMPROC_EXPORT_TEMPLATE_DECLARATION
+        #define IMPROC_EXPORT_TEMPLATE_DEFINITION IMPROC_API
     #else                           // Using the DLL
         #define IMPROC_API __declspec(dllimport)
+        #define IMPROC_EXPORT_TEMPLATE_DECLARATION IMPROC_API
+        #define IMPROC_EXPORT_TEMPLATE_DEFINITION
     #endif
 #elif (defined(__GNUC__) && __GNUC__ >= 4) || defined(__clang__)
     #define IMPROC_API __attribute__ ((visibility ("default")))
+    #define IMPROC_EXPORT_TEMPLATE_DECLARATION IMPROC_API
+    #define IMPROC_EXPORT_TEMPLATE_DEFINITION
 #else
     #define IMPROC_API
+    #define IMPROC_EXPORT_TEMPLATE_DECLARATION
+    #define IMPROC_EXPORT_TEMPLATE_DEFINITION
 #endif
 
 #define IMPROC_ENUM_KEY_TYPE unsigned int
